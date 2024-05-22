0f681b131cb9c02afb5e15743a385254e01355db
Gabriel Guimarães <ggabriel667@gmail.com>
2024-05-21 21:41:07 -0400

    Adicionado função de pesquisar veiculos

--- scripts/novoanuncio.js
+++ scripts/novoanuncio.js
@@ -1,18 +1,20 @@
 async function dadosItem() {
-  let formHtml = `
-  <label for="nome">Nome</label>
+  let htmlinterno = `
   <textarea id="nome" class="swal2-textarea" placeholder="Digite o nome do carro" aria-label="Digite o nome do carro"></textarea>
-  <label for="desc">Descrição</label>
+  <br>
+
   <textarea id="desc" class="swal2-textarea" placeholder="Digite a descrição do carro" aria-label="Digite a descrição do carro"></textarea>
-  <label for="imagem">Imagem</label>
+  <br>
+
   <textarea id="imagem" class="swal2-textarea" placeholder="Digite o local onde está a imagem" aria-label="Digite o local onde está a imagem"></textarea>
-`;
+  <br>
+  `;
 
   let nome, desc, imagem;
 
   await Swal.fire({
     title: 'Informações do Carro',
-    html: formHtml,
+    html: htmlinterno,
     focusConfirm: false,
     preConfirm: () => {
       return {
@@ -27,12 +29,23 @@ async function dadosItem() {
       nome = result.value.nome;
       desc = result.value.desc;
       imagem = result.value.imagem;
-      adicionarItem(nome, desc, imagem);
+      if(nome != "" && desc != "" && imagem != "")
+        {
+          adicionarItem(nome, desc, imagem);
+        }else{
+          
+            Swal.fire({
+              title: "Carro não adicionado",
+              text: "Existe informações não preenchidas!",
+              icon: "error"
+            });
+          
+        }
     } else if(!result.isConfirmed)
       {
         Swal.fire({
           title: "Carro não adicionado",
-          text: "O carro não foi adicionado a página",
+          text: "Operação cancelada pelo usuário!",
           icon: "error"
         });
       }
